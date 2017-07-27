b8238bdb1f20f82f3a69ce3a70d6c188df219f6e
tgruenewald <terrygruenewald@gmail.com>
2017-07-26 21:21:18 -0600

    entire finished game

---
+++ game/Assets/gamestate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class gamestate : MonoBehaviour {
+	public static int karma_count = 0;
+	bool restart_flag = false;
+	// Use this for initialization
+	void Start () {
+		GameObject.Find ("karma").GetComponent<Text> ().text = "Karma " + gamestate.karma_count;
+	}
+	IEnumerator gameover () {
+
+		yield return new WaitForSeconds (30);
+		GameObject.Find ("GameEnd").GetComponent<Text> ().enabled = true;
+		GameObject.Find ("GameEnd").GetComponent<Text> ().text = "Game Over!  Score " + karma_count;
+		restart_flag = true;
+
+
+	}
+	IEnumerator restart () {
+		yield return new WaitForSeconds (3);
+		GameObject.Find ("GameEnd").GetComponent<Text> ().enabled = false;
+		GameObject.Find ("karma").GetComponent<Text> ().text = "Karma " + gamestate.karma_count;
+		karma_count = 0;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (karma_count > 0) {
+			StartCoroutine(gameover());
+		}
+		if (restart_flag) {
+			restart_flag = false;
+			StartCoroutine (restart ());
+		}
+	}
+}
---
+++ game/Assets/grass.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class grass : MonoBehaviour {
+	Sprite[] stage = new Sprite[5]; // = Resources.Load<Sprite>("Sprites/droplet");
+
+	int i = 0;
+	Vector3 orig;
+
+	// Use this for initialization
+	void Start () {
+		stage[0] = Resources.Load<Sprite>("Sprites/dirt");
+		stage[1] = Resources.Load<Sprite>("Sprites/tilled_dirt");
+		stage[2] = Resources.Load<Sprite>("Sprites/seedling");
+		stage[3] = Resources.Load<Sprite>("Sprites/full_plant");
+
+		orig = GetComponent<Transform> ().localScale;
+		//GameObject.Find("karma").GetComponent<Text>().text = "Karma 50";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator MemeDestroy () {
+
+		yield return new WaitForSeconds (2);
+		i = 0;
+		GetComponent<BoxCollider2D> ().enabled = true;
+		GetComponent<SpriteRenderer> ().sprite = stage[i];
+		GetComponent<Transform> ().localScale = orig;
+
+	}
+	void OnMouseDown()
+	{
+		/*Do your stuff here*/
+
+		if (i == 4) {
+			gamestate.karma_count = gamestate.karma_count + Random.Range (1, 50);
+			GameObject.Find ("karma").GetComponent<Text> ().text = "Karma " + gamestate.karma_count;
+			GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Sprites/" +  Random.Range (1, 28));
+			GetComponent<Transform> ().localScale *= 6;
+			i = 0;
+			GetComponent<SpriteRenderer> ().sortingOrder = gamestate.karma_count;
+			GetComponent<BoxCollider2D> ().enabled = false;
+
+			StartCoroutine(MemeDestroy());
+		} else
+		{
+			GetComponent<SpriteRenderer> ().sprite = stage[i];
+			GetComponent<Transform> ().localScale = orig;
+		}
+		i++;
+
+
+
+	}
+
+}
